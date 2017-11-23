939d280a54eab8fc7ba5562f4b713f7ad6ba9a28
Krzysztof Rzymkowski <krzysztof.rzymkowski@lingaro.com>
2017-11-23 17:14:57 +0100

    json serialization

--- src/main/java/com/lingaro/web/WelcomeController.java
+++ src/main/java/com/lingaro/web/WelcomeController.java
@@ -1,5 +1,6 @@
 package com.lingaro.web;
 
+import com.lingaro.web.dto.Person;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +16,10 @@ public class WelcomeController {
     @GetMapping("mars")
     public String mars() {
         return "mars";
+    }
+
+    @GetMapping("/person")
+    public Person person() {
+        return new Person("Krzysztof", "Jeżyna");
     }
 }
---
+++ src/main/java/com/lingaro/web/dto/Person.java
@@ -0,0 +1,11 @@
+package com.lingaro.web.dto;
+
+public class Person {
+    public final String name;
+    public final String surname;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+}
