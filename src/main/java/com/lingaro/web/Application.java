71bf1df3eb061472faf0acea9bc4fa417a7d414e
Krzysztof Rzymkowski <krzysztof.rzymkowski@lingaro.com>
2017-11-23 17:00:38 +0100

    rest controller

--- src/main/java/com/lingaro/web/Application.java
+++ src/main/java/com/lingaro/web/Application.java
@@ -2,9 +2,17 @@ package com.lingaro.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Application {
+
+	@GetMapping("/")
+	public String hello() {
+		return "hello";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
